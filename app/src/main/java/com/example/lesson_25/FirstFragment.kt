5c11cd699f4f492a765a587c8b0e9c55384e6518
Mikhaylov-Andrey-derek23 <derek23@yandex.ru>
2024-03-03 13:08:44 +0300

    Lesson 26 class work

--- app/src/main/java/com/example/lesson_25/FirstFragment.kt
+++ app/src/main/java/com/example/lesson_25/FirstFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.lesson_25.PostClient.getPostsService
 import com.example.lesson_25.databinding.FragmentFirstBinding
@@ -17,7 +19,11 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class FirstFragment : Fragment() {
-    private  var bidding: FragmentFirstBinding? = null
+    private var bidding: FragmentFirstBinding? = null
+    private var adapter: PostAdapter? = null
+    private var mainPost: MainPost? = null
+    private var offset: Int = 0
+    private var postService: PostService? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,26 +31,55 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         bidding = FragmentFirstBinding.inflate(inflater, container, false)
-        return  bidding?.root
+        return bidding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val postService = getPostsService()
+        postService = getPostsService()
         val posts = CoroutineScope(Dispatchers.IO).async {
-            postService.getPicture()
+            postService?.getPicture(limit = 3, offset = offset)
         }
         CoroutineScope(Dispatchers.IO).launch {
-            val mainPost =  posts.await()
-
-            withContext(Dispatchers.Main){
-                Glide.with(requireContext())
-                    .load(mainPost.photos[0].url)
-                    .into(bidding!!.ivPicture)
-                bidding?.tvTitle?.text = mainPost.photos[0].title
-                bidding?.tvdescription?.text = mainPost.photos[0].description
+            mainPost = posts.await()
+            withContext(Dispatchers.Main) {
+                initPostAdapter()
             }
+
         }
     }
 
+    private fun initPostAdapter() {
+        adapter = PostAdapter()
+        bidding?.rvPost?.adapter = adapter
+        bidding?.rvPost?.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        adapter?.posts = mainPost!!.photos.toMutableList()
+        bidding?.rvPost?.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                val llm = bidding?.rvPost?.layoutManager as LinearLayoutManager
 
+                val lastVisiblePost = llm.findLastCompletelyVisibleItemPosition()
+
+                if (lastVisiblePost == adapter!!.posts.size - 1) {
+
+                    val previousSize = adapter!!.posts.size
+                    offset = adapter!!.posts.size
+
+                    val newPosts = CoroutineScope(Dispatchers.IO).async {
+                        postService?.getPicture(limit = 3, offset = offset)
+                    }
+
+                    CoroutineScope(Dispatchers.IO).launch {
+                        adapter?.posts?.addAll(newPosts.await()!!.photos)
+                        withContext(Dispatchers.Main) {
+                            adapter?.notifyItemRangeInserted(previousSize, 3)
+                        }
+                    }
+
+                }
+            }
+        })
+
+
+    }
 }
--- app/src/main/java/com/example/lesson_25/PostService.kt
+++ app/src/main/java/com/example/lesson_25/PostService.kt
@@ -1,9 +1,11 @@
 package com.example.lesson_25
 
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface PostService {
 
     @GET("v1/sample-data/photos")
-    suspend fun getPicture():MainPost
+
+    suspend fun getPicture(@Query("limit") limit:Int, @Query("offset") offset:Int):MainPost
 }
